9e99bbc3236cd163a6ab278767d6a8d45bad75a6
SKt1512 <sonu.k.work@gmail.com>
2025-11-03 14:45:02 +0530

    Adding first code

---
+++ code1.py
@@ -0,0 +1,13 @@
+#abc= input("Enter the string : ")
+abc =" This is a test string"
+length =len(abc)
+last_char = abc[-3:]
+if length >=3 :
+    if last_char.lower() =='ing':
+        new_str = abc+"ly"
+        print(new_str)
+    else:
+        new_str = abc+"ing"
+        print(new_str)
+else:
+    print("String length is short") 
